32e1cf73e97dfc00ce2330cbe54f51448f35f680
Nir-Elk <38635084+Nir-Elk@users.noreply.github.com>
2020-08-22 20:08:09 +0300

    .

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-const port = process.env.PORT || '8080';
-app.listen(port, () => console.log(`Running on localhost:${port}`));
